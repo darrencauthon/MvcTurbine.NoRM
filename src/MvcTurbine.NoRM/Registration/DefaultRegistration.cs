42c81c45f8a0603858381130123e4577e499ebd9
darrencauthon <darren@cauthon.com>
2010-08-13 22:06:13 -0500

    Add registration for mongo factory, get http handler to work.

---
+++ src/MvcTurbine.NoRM/Registration/DefaultRegistration.cs
@@ -0,0 +1,12 @@
+﻿using MvcTurbine.ComponentModel;
+
+namespace MvcTurbine.NoRM.Registration
+{
+    public class DefaultRegistration : IServiceRegistration
+    {
+        public void Register(IServiceLocator locator)
+        {
+            locator.Register<IMongoRepositoryFactory, MongoRepositoryFactory>();
+        }
+    }
+}

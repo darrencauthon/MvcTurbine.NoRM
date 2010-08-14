6b64df4f8d56dd0a69c006c61395db1887c1d312
darrencauthon <darren@cauthon.com>
2010-08-13 22:11:08 -0500

    Progress.

---
+++ src/MvcTurbine.NoRM.Tests/MongoRepositoryFactoryTests.cs
@@ -0,0 +1,23 @@
+﻿using AutoMoq;
+using NUnit.Framework;
+
+namespace MvcTurbine.NoRM.Tests
+{
+    [TestFixture]
+    public class MongoRepositoryFactoryTests
+    {
+        private AutoMoqer mocker;
+
+        [SetUp]
+        public void Setup()
+        {
+            mocker = new AutoMoqer();
+        }
+
+        [Test]
+        public void GetRepository_returns_a_repository_of_type_T()
+        {
+            Assert.Fail("START HERE");
+        }
+    }
+}

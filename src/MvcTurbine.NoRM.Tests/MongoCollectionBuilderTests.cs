ec2189528a1c50396f5a05b2a9b82e295ef13694
darrencauthon <darren@cauthon.com>
2010-08-15 09:14:24 -0500

    Create mongo collection builder.

---
+++ src/MvcTurbine.NoRM.Tests/MongoCollectionBuilderTests.cs
@@ -0,0 +1,35 @@
+﻿using AutoMoq;
+using Moq;
+using Norm;
+using Norm.Collections;
+using NUnit.Framework;
+using Should;
+
+namespace MvcTurbine.NoRM.Tests
+{
+    [TestFixture]
+    public class MongoCollectionBuilderTests
+    {
+        private AutoMoqer mocker;
+
+        [SetUp]
+        public void Setup()
+        {
+            mocker = new AutoMoqer();
+        }
+
+        [Test]
+        public void Returns_the_mongo_collection_from_the_mongo_object()
+        {
+            var expected = new Mock<IMongoCollection<Sample>>().Object;
+
+            mocker.GetMock<IMongo>()
+                .Setup(x => x.GetCollection<Sample>())
+                .Returns(expected);
+
+            var builder = mocker.Resolve<MongoCollectionBuilder>();
+            var result = builder.GetCollection<Sample>();
+            result.ShouldBeSameAs(expected);
+        }
+    }
+}
---
+++ src/MvcTurbine.NoRM/MongoCollectionBuilder.cs
@@ -0,0 +1,25 @@
+﻿using Norm;
+using Norm.Collections;
+
+namespace MvcTurbine.NoRM
+{
+    public interface IMongoCollectionBuilder
+    {
+        IMongoCollection<T> GetCollection<T>();
+    }
+
+    public class MongoCollectionBuilder : IMongoCollectionBuilder
+    {
+        private readonly IMongo mongo;
+
+        public MongoCollectionBuilder(IMongo mongo)
+        {
+            this.mongo = mongo;
+        }
+
+        public IMongoCollection<T> GetCollection<T>()
+        {
+            return mongo.GetCollection<T>();
+        }
+    }
+}
